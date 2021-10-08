abbfaf7de0702450edd6f9e03404f35f5f7026e2
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-10-08 11:38:30 +0200

    changed cyl to gear

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "XXX")
+wflow_publish(all = TRUE, message = "changed cyl to gear")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
