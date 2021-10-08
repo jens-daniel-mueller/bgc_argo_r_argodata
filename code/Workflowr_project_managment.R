14e380ec8d06abb3b6bb79211d32d4836cd4dd63
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-10-08 10:41:15 +0200

    manual commit

---
+++ code/Workflowr_project_managment.R
@@ -0,0 +1,27 @@
+# This script summarizes the central commands and steps to run an R project
+# using the Workflowr package.
+# For details please refer to:
+# https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
+
+# commit regular changes of modified .Rmd files(locally) and rebuild site
+wflow_publish(all = TRUE, message = "XXX")
+
+# commit changes including _site.yml (locally) and rebuild entire site in the specified order
+wflow_publish(here::here(
+  "analysis",
+  c(
+    "index.Rmd",
+    "access_data.Rmd"
+  )
+),
+message = "XXX",
+republish = TRUE)
+
+
+
+# Push latest version to GitHub
+wflow_git_push()
+
+jens-daniel-mueller
+
+pasqualina-vonlanthendinenna
