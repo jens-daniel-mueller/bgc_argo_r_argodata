81f5ac95754ecb913598df7344a2e6c627296add
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2021-10-13 12:01:18 +0200

    added timeseries of all 3 bgc variables

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,17 +4,20 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "changed cyl to gear")
+wflow_publish(all = TRUE, message = "added timeseries of all 3 bgc variables")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
   "analysis",
   c(
     "index.Rmd",
-    "access_data.Rmd"
+    "access_data.Rmd",
+    'plot_timeseries.Rmd',
+    'loading_data.Rmd',
+    'count_observations.Rmd'
   )
 ),
-message = "XXX",
+message = "added reading data in page",
 republish = TRUE)
 
 
