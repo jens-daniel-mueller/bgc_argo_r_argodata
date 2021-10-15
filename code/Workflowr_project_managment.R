895351d38b7a53cc3630c348072e94c857f97680
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2021-10-15 09:22:42 +0200

    changed fontsize in mapping titles

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,20 +4,19 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "added timeseries of all 3 bgc variables")
+wflow_publish(all = TRUE, message = "changed fontsize in mapping titles")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
   "analysis",
   c(
     "index.Rmd",
-    "access_data.Rmd",
-    'plot_timeseries.Rmd',
     'loading_data.Rmd',
-    'count_observations.Rmd'
+    'count_observations.Rmd',
+    'map_observations.Rmd'
   )
 ),
-message = "added reading data in page",
+message = "added map observations page",
 republish = TRUE)
 
 
