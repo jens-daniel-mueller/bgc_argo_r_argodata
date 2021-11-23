f43074c41fab2ba49de3ef503b9d3fbc59641c42
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2021-11-23 10:22:44 +0100

    switched reccap biomes to loading data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "updated timeseries plots")
+wflow_publish(all = TRUE, message = "switched reccap biomes to loading data")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
