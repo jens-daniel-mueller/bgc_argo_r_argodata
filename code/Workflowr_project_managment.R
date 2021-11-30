4b89d691dd5d070c282dd41048af1308f84ff8da
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2021-11-30 09:25:02 +0100

    added Argo plots to OceanSODA page

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "switched reccap biomes to loading data")
+wflow_publish(all = TRUE, message = "added Argo plots to OceanSODA page")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
@@ -15,10 +15,11 @@ wflow_publish(here::here(
     'coverage_timeseries.Rmd',
     'coverage_maps.Rmd',
     'ph_data.Rmd',
-    'oxygen_data.Rmd'
+    'oxygen_data.Rmd',
+    'oceanSODA_argo.Rmd'
   )
 ),
-message = "added oxygen data page",
+message = "added oceanSODA page",
 republish = TRUE)
 
 
