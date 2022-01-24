3f8e824c56d24899d7cce076ef23fe26219197d7
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-01-24 16:57:43 +0100

    updated 24/01

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "updated linear regression to monthly")
+wflow_publish(all = TRUE, message = "updated 24/01")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
