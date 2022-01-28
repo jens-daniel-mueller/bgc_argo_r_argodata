c0c12d0310fa0904f3ab32cdaf8331c26a52986a
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-28 23:37:01 +0100

    code cleaning: basinmask and regression

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "code review: basinmask and regression")
+wflow_publish(all = TRUE, message = "code cleaning: basinmask and regression")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
