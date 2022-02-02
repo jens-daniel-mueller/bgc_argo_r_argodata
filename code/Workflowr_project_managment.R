a73c7cf36f5960bd5520dd264fd0acbc8559f0ee
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-02-02 17:39:08 +0100

    changed to log scale and mean surface argo ph

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "changed map figure aspect")
+wflow_publish(all = TRUE, message = "changed to log scale and mean surface argo ph")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
