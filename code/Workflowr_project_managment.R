840030e9edcd68c2a4c675efed8ffbea702aa12e
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-02-28 11:00:29 +0100

    removed 2021 from SST comparison

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "plotted specific profiles")
+wflow_publish(all = TRUE, message = "removed 2021 from SST comparison")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
