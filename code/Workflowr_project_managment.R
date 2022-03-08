18dff1b06c6042d7b4a17d970b9121e78711ebd4
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-03-08 12:56:03 +0100

    subsetted profiles with flag A only for extremes

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "added January 2018 profile")
+wflow_publish(all = TRUE, message = "subsetted profiles with flag A only for extremes")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
