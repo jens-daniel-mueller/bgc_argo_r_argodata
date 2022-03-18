792f3f05324df194e6bc38b28fe499eb3ab0e3e5
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-03-18 10:32:19 +0100

    removed climatology from oceansoda temperature

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "updated to new only flag A data")
+wflow_publish(all = TRUE, message = "removed climatology from oceansoda temperature")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
