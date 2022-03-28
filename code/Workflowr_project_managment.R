c07ce4294f2199b712d98f3fbe9385051833b400
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-03-28 16:54:19 +0200

    rerun with mean instead of lm anomaly detection

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "rerun with lm instead of mean anomaly detection")
+wflow_publish(all = TRUE, message = "rerun with mean instead of lm anomaly detection")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
