c4d4031756aeaa94080b03c6d91103433e6f3e59
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-03-31 10:15:27 +0200

    extended OceanSODA to 1995 for extreme detection

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "updated figure aspects")
+wflow_publish(all = TRUE, message = "extended OceanSODA to 1995 for extreme detection")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
