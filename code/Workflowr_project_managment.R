9229c9dd9a20a180e4371ea3ef764b07292b2995
pasqualina-vonlanthendinenna <pasqualina.vonlanthendinenna@usys.ethz.ch>
2022-05-02 11:51:46 +0200

    write pH data to files

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -4,7 +4,7 @@
 # https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
 
 # commit regular changes of modified .Rmd files(locally) and rebuild site
-wflow_publish(all = TRUE, message = "code review: revised alternative double extreme color scale")
+wflow_publish(all = TRUE, message = "write pH data to files")
 
 # commit changes including _site.yml (locally) and rebuild entire site in the specified order
 wflow_publish(here::here(
@@ -14,10 +14,14 @@ wflow_publish(here::here(
     'loading_data.Rmd',
     'argo_clim_temp_csio.Rmd',
     'argo_clim_pH_ucsd.Rmd',
+    'load_OceanSODA.Rmd',
+    'load_biomes.Rmd',
+    'broullon_DIC_TA_clim.Rmd',
     'coverage_timeseries.Rmd',
     'coverage_maps.Rmd',
     'ph_data.Rmd',
     'oxygen_data.Rmd',
+    'temp_data.Rmd',
     'oceanSODA_argo.Rmd',
     'oceanSODA_argo_temp.Rmd',
     'extreme_pH.Rmd',
