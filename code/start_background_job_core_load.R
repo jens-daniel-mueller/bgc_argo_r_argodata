6d38b59f4446f0d7525c1d4ca90196f3c184be8d
ds2n19 <ds2n19@soton.ac.uk>
2023-10-12 12:06:37 +0200

    minor changes after JDM code review

--- code/start_background_job_core_load.R
+++ code/start_background_job_core_load.R
@@ -15,26 +15,28 @@
 # workflowr::wflow_publish(here::here(
 #   "analysis",
 #   c(
-#         'coverage_timeseries.Rmd',
-#         'coverage_maps.Rmd'
+#     'load_argo_core.Rmd'
 #   )
 # ),
-# message = "Changed core Argo location folders and run for 2013, 2014 and 2022")
+# message = "Run core Argo after code review 2013 - 2023")
 
 workflowr::wflow_publish(here::here(
   "analysis",
   c(
        'coverage_timeseries.Rmd',
-       'coverage_maps.Rmd'
+       'coverage_maps.Rmd',
+       'argo_temp_core.Rmd',
+       'oceanSODA_argo_temp.Rmd',
+       'extreme_temp_core.Rmd'
   )
 ),
-message = "refresh of coverage statistics to include full bcg and code loads 2013 - 2023")
-
-# Push latest version to GitHub
-wflow_git_push()
-
-jens-daniel-mueller
-
-pasqualina-vonlanthendinenna
-
-ds2n19
+message = "refresh coverage and analysis after full core load 2013 - 2023")
+#
+# # Push latest version to GitHub
+# wflow_git_push()
+#
+# jens-daniel-mueller
+#
+# pasqualina-vonlanthendinenna
+#
+# ds2n19
