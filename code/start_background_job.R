3f6800f3dc7640ac1eff95d77482c18a5d98296c
ds2n19 <ds2n19@soton.ac.uk>
2023-10-14 10:48:34 +0200

    Changes to core load to create a simple qc flag summary

--- code/start_background_job.R
+++ code/start_background_job.R
@@ -7,8 +7,19 @@ workflowr::wflow_publish(here::here(
     'extreme_pH.Rmd',
     'extreme_temp.Rmd',
     'extreme_temp_core.Rmd',
-    'extreme_compound.Rmd'
+    'extreme_compound.Rmd',
+    'variability_temp.Rmd',
+    'variability_pH.Rmd'
   )
 ),
-message = "Revised version of OceanSODA product -v2023")
+message = "Revised version of OceanSODA product -v2023 refresh after full core and BGC loads")
 
+#
+# # Push latest version to GitHub
+# wflow_git_push()
+#
+# jens-daniel-mueller
+#
+# pasqualina-vonlanthendinenna
+#
+# ds2n19
--- code/start_background_job_core_load.R
+++ code/start_background_job_core_load.R
@@ -12,25 +12,25 @@
 # ),
 # message = "Changed core Argo location folders and run for 2013-2015")
 
-# workflowr::wflow_publish(here::here(
-#   "analysis",
-#   c(
-#     'load_argo_core.Rmd'
-#   )
-# ),
-# message = "Run core Argo after code review 2013 - 2023")
-
 workflowr::wflow_publish(here::here(
   "analysis",
   c(
-       'coverage_timeseries.Rmd',
-       'coverage_maps.Rmd',
-       'argo_temp_core.Rmd',
-       'oceanSODA_argo_temp.Rmd',
-       'extreme_temp_core.Rmd'
+    'load_argo_core.Rmd'
   )
 ),
-message = "refresh coverage and analysis after full core load 2013 - 2023")
+message = "Added load qc metrics")
+
+# workflowr::wflow_publish(here::here(
+#   "analysis",
+#   c(
+#        'coverage_timeseries.Rmd',
+#        'coverage_maps.Rmd',
+#        'argo_temp_core.Rmd',
+#        'oceanSODA_argo_temp.Rmd',
+#        'extreme_temp_core.Rmd'
+#   )
+# ),
+# message = "refresh coverage and analysis after full core load 2013 - 2023")
 #
 # # Push latest version to GitHub
 # wflow_git_push()
